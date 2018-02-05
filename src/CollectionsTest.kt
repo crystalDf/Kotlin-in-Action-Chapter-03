072dcddaa2bc9327f8f71d8cf11f6111faff28ee
crystalDf <chendong333@gmail.com>
2018-02-05 23:00:15 +0800

    Chapter 03

--- src/CollectionsTest.kt
+++ src/CollectionsTest.kt
@@ -1,3 +1,8 @@
+import strings.join
+import strings.joinToString
+import strings.joinToString2
+import strings.lastChar as last
+
 fun main(args: Array<String>) {
 
     val set = hashSetOf(1, 7, 53)
@@ -13,4 +18,16 @@ fun main(args: Array<String>) {
 
     val numbers = setOf(1, 14, 2)
     println(numbers.max())
+
+    println(list)
+
+    println(joinToString(list, separator = "; ", prefix = "(", postfix = ")"))
+    println(joinToString(list))
+    println(joinToString(list, separator = "; "))
+    println(joinToString(list, postfix = ";", prefix = "# "))
+
+    println("Kotlin".last())
+
+    println(list.joinToString2(separator = "; ", prefix = "(", postfix = ")"))
+    println(strings.join())
 }
---
+++ src/strings/Join.kt
@@ -0,0 +1,68 @@
+package strings
+
+fun <T> joinToString(
+        collection: Collection<T>,
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
+
+fun <T> Collection<T>.joinToString2(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
+
+fun String.lastChar(): Char = this[this.length - 1]
+
+fun String.lastChar2(): Char = get(length - 1)
+
+fun Collection<String>.join(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String = joinToString2(separator, prefix, postfix)
+
+val String.lastChar: Char
+    get() = get(length - 1)
+
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value) {
+        setCharAt(length - 1, value)
+    }
+
+fun main(args: Array<String>) {
+    println("Kotlin".lastChar())
+    println("Kotlin".lastChar2())
+    println("Kotlin".lastChar)
+
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
+
+    println(sb)
+}
