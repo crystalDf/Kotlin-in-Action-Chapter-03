ec7d3cac91d1c63e65a37295dfe7e8012b0c31a5
crystalDf <chendong333@gmail.com>
2018-02-08 21:27:22 +0800

    Chapter 03

---
+++ src/SaveUserTest.kt
@@ -0,0 +1,46 @@
+class User(val id: Int, val name: String, val address: String)
+
+fun User.validateBeforeSave() {
+
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user $id: empty $fieldName")
+        }
+    }
+
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+fun saveUser(user: User) {
+
+    if (user.name.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${user.id}: empty Name")
+    }
+
+    if (user.address.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${user.id}: empty Address")
+    }
+}
+
+fun saveUser2(user: User) {
+
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${user.id}: empty $fieldName")
+        }
+    }
+
+    validate(user.name, "Name")
+    validate(user.address, "Address")
+}
+
+fun saveUser3(user: User) {
+
+    user.validateBeforeSave()
+}
+
+fun main(args: Array<String>) {
+
+    saveUser3(User(1, "", ""))
+}
--- src/StringsAndRegularExpressionsTest.kt
+++ src/StringsAndRegularExpressionsTest.kt
@@ -2,4 +2,35 @@ fun main(args: Array<String>) {
 
     println("12.345-6.A".split("\\.|-".toRegex()))
     println("12.345-6.A".split(".", "-"))
+
+    fun parsePath(path: String) {
+
+        val directory = path.substringBeforeLast("/")
+        val fullName = path.substringAfterLast("/")
+
+        val fileName = fullName.substringBeforeLast(".")
+        val extension = fullName.substringAfterLast(".")
+
+        println("Dir: $directory, name: $fileName, extension: $extension")
+    }
+
+    parsePath("/Users/star/kotlin-book/chapter.adoc")
+
+    fun parsePath2(path: String) {
+
+        val regex = """(.+)/(.+)\.(.+)""".toRegex()
+        val matchResult = regex.matchEntire(path)
+
+        if (matchResult != null) {
+            val (directory, fileName, extension) = matchResult.destructured
+            println("Dir: $directory, name: $fileName, extension: $extension")
+        }
+    }
+
+    parsePath2("/Users/star/kotlin-book/chapter.adoc")
+
+    val kotlinLogo = """| //
+                       .|//
+                       .|/ \"""
+    println(kotlinLogo.trimMargin("."))
 }
